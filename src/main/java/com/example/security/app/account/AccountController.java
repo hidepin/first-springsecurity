213fd4678aa139ccb235b1545d63838832087d3d
Hidetoshi Imai <hidepin@gmail.com>
2018-05-18 21:24:20 +0900

    Add RequestMapping

--- src/main/java/com/example/security/app/account/AccountController.java
+++ src/main/java/com/example/security/app/account/AccountController.java
@@ -3,10 +3,11 @@ package com.example.security.app.account;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-//@Controller
+@Controller
+@RequestMapping("/login")
 public class AccountController {
 
-//	@RequestMapping("loginForm")
+	@RequestMapping("loginForm")
 	String loginForm() {
 		return "login/loginForm";
 	}
